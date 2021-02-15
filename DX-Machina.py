0816b810cc94fb7243625be8026d2b9bfe37f142
dmhrpr <79041532+dmhrpr@users.noreply.github.com>
2021-02-15 12:52:35 +0000

    Update DX-Machina.py

--- DX-Machina.py
+++ DX-Machina.py
@@ -25,7 +25,7 @@ msg = mido.Message
 def main():
     audio_input = CHOOSE_INPUT_DEVICE()
     port = SETUP_MIDI()
-    print('Choose the audio sample you want to mimic.')
+    print('\nChoose the audio sample you want to mimic.\n')
     sleep(1)
     Tk().withdraw()
     audio_file = askopenfilename()
@@ -40,9 +40,11 @@ def main():
     MUTATE(gen)
 
     while True():
+        gen = gen + 1
+        gen_name = str(gen).zfill(4)
+        print(f'Start of Generation {gen_name}.\n')
         RECORD_PATCHES(port, duration, audio_input)
         MFCC(audio_file_data)
-        gen = gen + 1
         MUTATE(gen)
 
 
@@ -73,7 +75,7 @@ def GET_AUDIO_FILE_LENGTH(audio_file):
     with contextlib.closing(wave.open(audio_file, 'r')) as f:
         frames = f.getnframes()
         rate = f.getframerate()
-        duration = round(frames / float(rate), 1)
+        duration = frames / float(rate)
 
     return duration
 
@@ -132,10 +134,14 @@ def INIT_PATCHES():
 
 
 def RECORD_PATCHES(port, duration, audio_input):
+    print('Recording patches...\n')
+    
     patches = [s for s in glob.glob('*.syx') if s.startswith('p')]
 
     for x in patches:
         RECORDER(x, port, duration, audio_input)
+          
+    print('Done!\n')      
 
 
 def RECORDER(x, port, duration, audio_input):
@@ -176,7 +182,7 @@ def RECORDER(x, port, duration, audio_input):
 
     p.terminate()
 
-    print(f'Finished recording {x}')
+    print(f'Finished recording {x}\n')
 
     wf = wave.open(x.split('.')[0] + '.wav', 'wb')
     wf.setnchannels(channels)
@@ -216,11 +222,11 @@ def MFCC(audio_file_data):
         os.rename(x.split(".")[0] + ".syx",
                   (str(avg).replace(".", "")).ljust(4, "0") + ".syx")
 
-    print('Done!')
+    print('Done!\n')
 
 
 def MUTATE(gen):
-    print('Creating new generation of patches...'
+    print('Forming new generation...'
 
     prev_gen_syx = sorted([s for s in glob.glob("*.syx") if s[0].isnumeric()])
     prev_gen_wav = sorted([s for s in glob.glob("*.wav") if s[0].isnumeric()])
@@ -232,7 +238,8 @@ def MUTATE(gen):
 
     for x in new_gen:
         patch_no = patch_no + 1
-        prev_syx_file = mido.read_syx_file(match)[1:6]
+          
+        prev_syx_file = mido.read_syx_file(x)[1:6]
 
         header = [67, 0, 127, 28, 0, 4, 5, 14, 15, 0, 94]
         com = [int(s) for s in re.findall(r'\b\d+\b', str(prev_syx_file[0])[12:-8])][:-1]
@@ -260,95 +267,47 @@ def MUTATE(gen):
 
         op_params = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 24, 27, 28, 29, 34]
         
-        for param in op_params:
-            p = op1[param]
-            if p >= 102:
-                op1[param] = (p - 50) + r(0, 50)
-            elif p <= 25:
-                op1[param] = p + r(0, 50)
-            else:
-                op1[param] = (p - 25) + r(0, 50)
+        operators = [op1, op2, op3, op4]
+        
+        for op in operators:  
+            for param in op_params:
+                p = op[param]
+                if p >= 102:
+                    op[param] = (p - 50) + r(0, 50)
+                elif p <= 25:
+                    op[param] = p + r(0, 50)
+                else:
+                    op[param] = (p - 25) + r(0, 50)
 
-            p = op2[param]
-            if p >= 102:
-                op2[param] = (p - 50) + r(0, 50)
-            elif p <= 25:
-                op2[param] = p + r(0, 50)
-            else:
-                op2[param] = (p - 25) + r(0, 50)
-
-            p = op2[param]
-            if p >= 102:
-                op2[param] = (p - 50) + r(0, 50)
-            elif p <= 25:
-                op2[param] = p + r(0, 50)
-            else:
-                op2[param] = (p - 25) + r(0, 50)
-
-            p = op1[param]
-            if p >= 102:
-                op1[param] = (p - 50) + r(0, 50)
-            elif p <= 25:
-                op1[param] = p + r(0, 50)
-            else:
-                op1[param] = (p - 25) + r(0, 50)
-
-        op1[22] = r(0, 3)
-        op1[23] = r(0, 3)
-        op1[25] = r(0, 1)
-        op1[26] = r(0, 1)
-        op1[30] = r(0, 1)
-        op1[31] = r(0, 1)
-        op1[32] = r(0, 31)
-        op1[33] = r(0, 99)
-
-        op2[22] = r(0, 3)
-        op2[23] = r(0, 3)
-        op2[25] = r(0, 1)
-        op2[26] = r(0, 1)
-        op2[30] = r(0, 1)
-        op2[31] = r(0, 1)
-        op2[32] = r(0, 31)
-        op2[33] = r(0, 99)
-
-        op3[22] = r(0, 3)
-        op3[23] = r(0, 3)
-        op3[25] = r(0, 1)
-        op3[26] = r(0, 1)
-        op3[30] = r(0, 1)
-        op3[31] = r(0, 1)
-        op3[32] = r(0, 31)
-        op3[33] = r(0, 99)
-
-        op4[22] = r(0, 3)
-        op4[23] = r(0, 3)
-        op4[25] = r(0, 1)
-        op4[26] = r(0, 1)
-        op4[30] = r(0, 1)
-        op4[31] = r(0, 1)
-        op4[32] = r(0, 31)
-        op4[33] = r(0, 99)
+            op[22] = r(0, 3)
+            op[23] = r(0, 3)
+            op[25] = r(0, 1)
+            op[26] = r(0, 1)
+            op[30] = r(0, 1)
+            op[31] = r(0, 1)
+            op[32] = r(0, 31)
+            op[33] = r(0, 99)
  
-        voice_syx = [com, op1, op2, op3, op4]
+        voice_settings = [com, op1, op2, op3, op4]
 
-        for syx in voice_syx:
-            data = syx[6:]
-            checksum = (sum(data)) % 128
-            if checksum == 127:
-                checksum = checksum
-            elif checksum == 0:
-                checksum = checksum
+        for y in voice_settings:
+            parameters = y[6:]
+            c_sum = (sum(parameters)) % 128
+            if c_sum == 127:
+                c_sum = c_sum
+            elif c_sum == 0:
+                c_sum = c_sum
             else:   
-                checksum = 128 - checksum
-            syx.append(checksum)
+                c_sum = 128 - c_sum
+            y.append(c_sum)
 
-        header = mido.Message("sysex", data = header)
-        com = mido.Message("sysex", data = com)
-        op1 = mido.Message("sysex", data = op1)
-        op2 = mido.Message("sysex", data = op2)
-        op3 = mido.Message("sysex", data = op3)
-        op4 = mido.Message("sysex", data = op4)
-        footer = mido.Message("sysex", data = footer)
+        header = msg("sysex", data=header)
+        com = msg('sysex', data=com)
+        op1 = msg('sysex', data=op1)
+        op2 = msg('sysex', data=op2)
+        op3 = msg('sysex', data=op3)
+        op4 = msg('sysex', data=op4)
+        footer = msg('sysex', data=footer)
 
         syx_file = [header, com, op1, op2, op3, op4, footer]
             
@@ -364,7 +323,7 @@ def MUTATE(gen):
     for i in prev_gen_wav:
         os.rename(i, new_folder + '/' + i)
 
-    print('Done!')
+    print('Done!\n')
 
 
 if __name__ == '__main__':
